c3bd420423bf8aeda53d442a45edb3317071fb1b
Keyper <kcybulski93@gmail.com>
2020-05-25 11:12:52 +0200

    Updates

---
+++ precache-manifest.ca196f7eb1c619cafb70581e1eb2945f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a3c0d785d6984372fa9491ad77b913f",
+    "url": "/To-Do-App/index.html"
+  },
+  {
+    "revision": "153b5ba7ddb1cc9382f3",
+    "url": "/To-Do-App/static/css/main.9d66ab2c.chunk.css"
+  },
+  {
+    "revision": "013d33a54e98411608cb",
+    "url": "/To-Do-App/static/js/2.99b024ad.chunk.js"
+  },
+  {
+    "revision": "153b5ba7ddb1cc9382f3",
+    "url": "/To-Do-App/static/js/main.e3f15a46.chunk.js"
+  },
+  {
+    "revision": "5529420073f57fa76f56",
+    "url": "/To-Do-App/static/js/runtime~main.08825c03.js"
+  },
+  {
+    "revision": "fcb1692c32e9335e653f644cfb13d144",
+    "url": "/To-Do-App/static/media/symphony.fcb1692c.png"
+  }
+]);
